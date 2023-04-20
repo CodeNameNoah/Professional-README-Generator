2e1e7b427eadb55d25b489a0e2c7b442009ead31
Noah Hoang <hoincbusiness@gmail.com>
2023-04-19 23:46:49 -0700

    Created a function to generate markdown for README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# undefined
-
+# 
+  https://github.com//
+  ## Description
+  
+  ## Table of Contents 
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributors](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation
+  The following necessary dependencies must be installed to run the application properly: 
+  ## Usage
+  In order to use this app, 
+  ## License
+  This project is licensed under the  license. 
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+  ## Contributors
+  ​Contributors: undefined
+  ## Tests
+  The following is needed to run the test: 
+  ## Questions
+  If you have any questions about the repo, open an issue or contact  directly at : .
+  
--- util/generateMarkdown.js
+++ util/generateMarkdown.js
@@ -11,10 +11,33 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+// ? DONE
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.Title}
+  https://github.com/${data.Username}/${data.Title}
+  ## Description
+  ${data.Description}
+  ## Table of Contents 
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributors](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation
+  The following necessary dependencies must be installed to run the application properly: ${data.Installation}
+  ## Usage
+  In order to use this app, ${data.Usage}
+  ## License
+  This project is licensed under the ${data.License} license. 
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+  ## Contributors
+  ​Contributors: ${data.Contributors}
+  ## Tests
+  The following is needed to run the test: ${data.Tests}
+  ## Questions
+  If you have any questions about the repo, open an issue or contact ${data.Username} directly at : ${data.Email}.
+  `;
 }
 
 module.exports = generateMarkdown;
